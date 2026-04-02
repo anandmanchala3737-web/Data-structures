834fc3de2cc158fc07e986486d315b4c1cb271c0
Anand <anandmanchala3737@gmail.com>
2026-04-02 23:07:51 +0530

    First Tree

--- tree.c
+++ tree.c
@@ -1,1 +1,33 @@
-#include<>
+#include<stdio.h>
+
+struct root{
+    int root;
+    struct root *right;
+    struct root *left;
+};
+
+void create_tree(int val){
+    struct root *root;
+    root=(struct root*)malloc(sizeof(struct root));
+    if(root==NULL){
+        root=val;
+    }
+
+    else if(root>val){
+        root->left=val;
+    }
+    else if(root<val){
+        root->right=val;
+    }
+}
+
+int main(){
+    int val;
+
+    printf("Enter the data:");
+    scanf("%d",&val);
+
+    create_tree(val);
+
+    return 0;
+}
